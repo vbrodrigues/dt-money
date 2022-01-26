1f6cfe9274e6b0117082ffa6982159eb8d842594
Vitor Rodrigues <vitorborbarodrigues@gmail.com>
2022-01-26 01:17:20 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header"
+import { GlobalStyle } from "./styles/global"
+
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
+  )
+}
+
+export default App
